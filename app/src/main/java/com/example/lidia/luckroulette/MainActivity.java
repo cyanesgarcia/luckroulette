f904b7cb0a5a12cfaef4a109b183414a192fed5e
lyanesgarcia <lyanesgarcia@my.apsu.edu>
2018-06-24 07:54:25 -0500

    update java and layout

--- app/src/main/java/com/example/lidia/luckroulette/MainActivity.java
+++ app/src/main/java/com/example/lidia/luckroulette/MainActivity.java
@@ -17,39 +17,56 @@ import java.util.Random;
 
 public class MainActivity extends Activity implements Animation.AnimationListener{
     boolean blnButtonRotation =true;
-    int intNumber= 6;
+    int intNumber= 8;
     long lngDegrees=0;
     SharedPreferences sharedPreferences;
 
+    ImageView iv_wheel;
+
+    String color= " ";
     Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().addFlags(1024);
-        requestWindowFeature(1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button);
         this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        this.intNumber = this.sharedPreferences.getInt("INT_NUMBER", 6);
-
+        this.intNumber = this.sharedPreferences.getInt("INT_NUMBER", 8);
+        iv_wheel= (ImageView) findViewById(R.id.imageView2);
 
     }
 
     @Override
     public void onAnimationStart(Animation animation) {
-this.blnButtonRotation= false;
-button.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void onAnimationEnd(Animation animation) {
- Toast toast=Toast.makeText(this, " "+String.valueOf((int)(((double)this.intNumber)
-         -Math.floor(((double)this.lngDegrees)/(360.0d/(double)this.intNumber))))+" ",0);
- toast.setGravity(49,0,0);
- toast.show();
- this.blnButtonRotation=true;
- button.setVisibility(View.VISIBLE);
+        String number= String.valueOf((int)(((double)this.intNumber)
+                -Math.floor(((double)this.lngDegrees)/(360.0d/(double)this.intNumber))));
+        if (number.equals("1")){
+            color="Red";
+        }else if(number.equals("2")){
+            color="Orange";
+        }else if(number.equals("3")){
+            color="Yellow";
+        }else if(number.equals("4")){
+            color="Light Green";
+        }else if(number.equals("5")){
+            color="Dark Green";
+        }else if(number.equals("6")){
+            color="Light Blue";
+        }else if(number.equals("7")){
+            color="Dark Blue";
+        }else if(number.equals("8")){
+            color="Purple";
+        }
+        Toast toast=Toast.makeText(this, color,0);
+        toast.setGravity(49,0,0);
+        toast.show();
+        this.blnButtonRotation=true;
+        button.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -59,19 +76,15 @@ button.setVisibility(View.VISIBLE);
 
     public void onClickButtonRotation(View view)
     {
-        if(this.blnButtonRotation)
-        {
-            int ran= new Random().nextInt(360)+3600;
-            RotateAnimation rotateAnimation= new RotateAnimation((float)this.lngDegrees, (float)(this.lngDegrees+ ((long)ran)),1,0.5f,1,0.5f);
+        int ran= new Random().nextInt(360)+3600;
+        RotateAnimation rotateAnimation= new RotateAnimation((float)this.lngDegrees, (float)(this.lngDegrees+ ((long)ran)),1,0.5f,1,0.5f);
 
-            this.lngDegrees=(this.lngDegrees + ((long)ran))%360;
-            rotateAnimation.setDuration((long)ran);
-            rotateAnimation.setFillAfter(true);
-            rotateAnimation.setInterpolator(new DecelerateInterpolator());
-            rotateAnimation.setAnimationListener(this);
-
-
-        }
+        this.lngDegrees=(this.lngDegrees + ((long)ran))%360;
+        rotateAnimation.setDuration((long)ran);
+        rotateAnimation.setFillAfter(true);
+        rotateAnimation.setInterpolator(new DecelerateInterpolator());
+        rotateAnimation.setAnimationListener(this);
+        iv_wheel.startAnimation(rotateAnimation);
     }
 }
 
